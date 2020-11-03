edad2991dccd191ed0acf806f2a6d4a4e1c4b482
Hlibov <gglebov777@gmail.com>
2020-11-03 13:26:00 +0200

    create class constructop for create cards in HTML

--- js/main.js
+++ js/main.js
@@ -108,7 +108,7 @@ const listBtnModal = document.querySelectorAll('[data-modal]'),
 function toggleShow(strValue = 'hidden') {
   modalWindow.classList.toggle('show');
   document.body.style.overflow = strValue;
-  clearInterval(modalTimerId);
+  // clearInterval(modalTimerId);
 }
 
 // ф-я обработчика события на кнопки для вызова модального окна
@@ -132,7 +132,7 @@ document.addEventListener('keydown', (e) => {
   }
 });
 
-const modalTimerId = setTimeout(toggleShow, 6000);
+// const modalTimerId = setTimeout(toggleShow, 6000);
 
 //ф-я включения модального окна при конечном скроле страницы и затем удаляет слушетель события на скролл этой функции
 function showModalByScroll() {
@@ -144,3 +144,55 @@ function showModalByScroll() {
 }
 //слушатель события на скролл
 window.addEventListener('scroll', showModalByScroll);
+
+//Используем классы для карточек
+
+class MenuCard {
+  constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+    this.src = src;
+    this.alt = alt;
+    this.title = title;
+    this.descr = descr;
+    this.price = price;
+    this.parent = document.querySelector(parentSelector);
+    this.classes = classes;
+    this.transfer = 27;
+    this.changeTOUAH();
+  }
+
+  changeTOUAH() {
+    this.price = this.price * this.transfer;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    if (this.classes.length === 0) {
+      this.element = 'menu_item';
+      element.classList.add(this.element);
+    } else {
+      this.classes.forEach(itemClass => element.classList.add(itemClass));
+    }
+    element.innerHTML = `
+    <div class="menu__item">
+    <img src=${this.src} alt=${this.alt}>
+    <h3 class="menu__item-subtitle">${this.title}</h3>
+    <div class="menu__item-descr">${this.descr}</div>
+    <div class="menu__item-divider"></div>
+    <div class="menu__item-price">
+      <div class="menu__item-cost">Цена:</div>
+      <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+    </div>
+  </div>
+  `;
+    this.parent.append(element);
+  }
+}
+
+const div = new MenuCard('img/tabs/vegy.jpg', 'vegy', 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей ифруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 9, '.menu .container');
+div.render();
+
+const div1 = new MenuCard('img/tabs/elite.jpg', 'elite', 'Меню “Премиум”', 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд.Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 10, '.menu .container');
+div1.render();
+
+const div2 = new MenuCard('img/tabs/post.jpg', 'post', 'Меню "Постное"', 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', 8, '.menu .container');
+div2.render();
